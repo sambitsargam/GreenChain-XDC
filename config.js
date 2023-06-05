629ceb6fe106d632c81ddaa1e9a34350c12310cf
SAMBIT SARGAM EKALABAYA <70655824+sambitsargam@users.noreply.github.com>
2023-06-05 23:24:24 +0530

    update

---
+++ config.js
@@ -0,0 +1,2 @@
+export const wastemarketplaceAddress = "xdc9E6Fac91D2C7627FC473aE57bF82067FC4a4304e";
+
---
+++ src/assets/App.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line import/no-unresolved
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
+
+const App = () => (
+  <div className="min-h-screen">
+    <div className="bg-gradient-to-r from-blue-800 to-black">
+      <Navbar />
+      <Welcome />
+    </div>
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
+
+export default App;
