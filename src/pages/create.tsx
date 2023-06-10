3c2d8ca8b3f1a242a975f58515c90c5338dd8eac
SAMBIT SARGAM EKALABAYA <70655824+sambitsargam@users.noreply.github.com>
2023-06-10 21:20:58 +0530

    fixing-bugs

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,55 +1,59 @@
 import React from "react";
 import { Navbar, MintProfile } from "../components";
 import { CivicPassProvider } from "../context/CivicPass";
-import { GatewayStatus, IdentityButton, useGateway } from "@civic/ethereum-gateway-react";
-import {useAccount, configureChains, createClient, WagmiConfig } from "wagmi";
-import { publicProvider } from 'wagmi/providers/public';
-import { connectorsForWallets, getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { hardhat } from 'wagmi/chains';
-import { xdcTestnet } from '../context/xdc';
-
-
-const Interface = () => {
-  <div className="bg-gradient-to-b from-green-700 to-black">
-    <Navbar />
-    <MintProfile />
-  </div>
-}
+import {
+  GatewayStatus,
+  IdentityButton,
+  useGateway,
+} from "@civic/ethereum-gateway-react";
+import { useAccount, configureChains, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import {
+  connectorsForWallets,
+  getDefaultWallets,
+  RainbowKitProvider, ConnectButton,
+} from "@rainbow-me/rainbowkit";
+import { hardhat } from "wagmi/chains";
+import { xdcTestnet } from "../context/xdc";
 
 const clientChains = [xdcTestnet, hardhat];
 const { chains, provider } = configureChains(clientChains, [publicProvider()]);
 
-const { wallets } = getDefaultWallets({ appName: 'Green Chain', chains });
+const { wallets } = getDefaultWallets({ appName: "Green Chain", chains });
 
-const connectors = connectorsForWallets([
-    ...wallets,
-]);
+const connectors = connectorsForWallets([...wallets]);
 const client = createClient({
-    autoConnect: true,
-    connectors,
-    provider,
+  autoConnect: true,
+  connectors,
+  provider,
 });
 
 const Gateway = () => {
   const { gatewayStatus } = useGateway();
-  const { address } = useAccount();
-if (!address) return <div>Connect your wallet</div>;
-  return ( 
-    <div>
+  return (
+    <div className="object-center" >
       <IdentityButton />
-        { gatewayStatus !== GatewayStatus.ACTIVE && <div>Verify you are a unique person before entering</div>}
+      {gatewayStatus !== GatewayStatus.ACTIVE && (
+        <div>Verify you are a unique person before entering</div>
+      )}
     </div>
-    )
-  }
+  );
+};
 
 function Create() {
   return (
-    <WagmiConfig client={client}>
-    <CivicPassProvider>
-        <Gateway />
-        <Interface />
-    </CivicPassProvider>
-    </WagmiConfig>
+    <div className="bg-gradient-to-b from-green-700 to-black">
+      <WagmiConfig client={client}>
+        <RainbowKitProvider chains={clientChains} modalSize="compact">
+          <CivicPassProvider>
+            <Navbar />
+            <ConnectButton />
+            <Gateway />
+            <MintProfile />
+          </CivicPassProvider>
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </div>
   );
 }
 
